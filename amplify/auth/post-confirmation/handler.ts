331f4c724eb15b548aff45803f27f9e0a57d5046
isagwong-ux <isagwong@icloud.com>
2026-03-13 23:05:12 -0500

    Update handler.ts

--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -1,6 +1,5 @@
-import { PostConfirmationTriggerHandler } from "aws-lambda";
-
-export const handler: PostConfirmationTriggerHandler = async (event) => {
-  console.log("User confirmed:", event.request.userAttributes);
+import type { PostConfirmationTriggerHandler } from "aws-lambda"; 
+export const handler: PostConfirmationTriggerHandler = async (event) => { 
   return event;
 };
+
